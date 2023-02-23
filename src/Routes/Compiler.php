62032be4b6ff4cbdcd8b60eb754de2b0d3fcfa19
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-02-23 21:40:46 +0100

    list files

--- src/Routes/Compiler.php
+++ src/Routes/Compiler.php
@@ -16,6 +16,29 @@ class Read implements IRoute{
             $db->direct('SET SESSION group_concat_max_len = 4294967295;');
             try{
                 App::result('data', dirname(__DIR__,5));
+
+                $allfiles = [];
+                foreach (glob(dirname(__DIR__,5).'/cmp/*/compile.json') as $filename) {
+                    $list = json_decode(file_get_contents($filename),true);
+                    $localindex = 0;
+                    if (!is_array($list)) continue;
+                    foreach($list as $entry){
+                        if (is_array($entry)){
+                            $allfiles[] = [
+                                'filename'=>dirname($filename).'/'.$entry[0],
+                                'priority'=>sprintf('%06d',intval($entry[1])).sprintf('%06d',$localindex)
+                            ];
+                        }else{
+                            $allfiles[] = [
+                                'filename'=>dirname($filename).'/'.$entry,
+                                'priority'=>sprintf('%06d',99999).sprintf('%06d',$localindex)
+                            ];
+                        }
+                        $localindex++;
+                    }
+                }
+                App::result('compile', $allfiles);
+                
                 App::result('success', true);
             }catch(\Exception $e){
                 App::result('msg', $e->getMessage());

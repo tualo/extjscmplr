0f1a79c3aa86c9b94a13ef71a28133495e5b2ba8
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-19 09:09:03 +0100

    test path

--- src/Routes/Ui.php
+++ src/Routes/Ui.php
@@ -9,17 +9,88 @@ use Tualo\Office\ExtJSCompiler\Helper;
 
 class Ui implements IRoute{
 
+    public static function readfile($filename, $identifier = "", $strict = false){
+        // check: are we still allowed to send headers?
+        if (headers_sent()) {
+            exit(404);
+        }
+
+        $stat = stat($filename);
+        $timestamp = $stat['mtime'];
+
+        // get: header values from client request
+        $client_etag =
+            !empty($_SERVER['HTTP_IF_NONE_MATCH'])
+            ?   trim($_SERVER['HTTP_IF_NONE_MATCH'])
+            :   null
+        ;
+        $client_last_modified =
+            !empty($_SERVER['HTTP_IF_MODIFIED_SINCE'])
+            ?   trim($_SERVER['HTTP_IF_MODIFIED_SINCE'])
+            :   null
+        ;
+        $client_accept_encoding =
+            isset($_SERVER['HTTP_ACCEPT_ENCODING'])
+            ?   $_SERVER['HTTP_ACCEPT_ENCODING']
+            :   ''
+        ;
+
+        /**
+         * Notes
+         *
+         * HTTP requires that the ETags for different responses associated with the 
+         * same URI are different (this is the case in compressed vs. non-compressed
+         * results) to help caches and other receivers disambiguate them.
+         *
+         * Further we cannot trust the client to always enclose the ETag in normal
+         * quotation marks (") so we create a "raw" server sided ETag and only
+         * compare if our ETag is found in the ETag sent from the client
+         */
+
+        // calculate: current/new header values
+        $server_last_modified = gmdate('D, d M Y H:i:s', $timestamp) . ' GMT';
+        $server_etag_raw = md5($timestamp . $client_accept_encoding . $identifier);
+        $server_etag = '"' . $server_etag_raw . '"';
+
+        // calculate: do client and server tags match?
+        $matching_last_modified = $client_last_modified == $server_last_modified;
+        $matching_etag = $client_etag && strpos($client_etag, $server_etag_raw) !== false;
+
+        // set: new headers for cache recognition
+        header('Last-Modified: ' . $server_last_modified);
+        header('ETag: ' . $server_etag);
+
+        // check: are client and server headers identical ("no changes")?
+        if (
+            ($client_last_modified && $client_etag) || $strict
+            ?   $matching_last_modified && $matching_etag
+            :   $matching_last_modified || $matching_etag
+        ) {
+            header($_SERVER['SERVER_PROTOCOL'] . ' 304 Not Modified');
+            exit(304);
+        }
+
+       readfile($filename);
+    }
+
+
     public static function register(){
-        BasicRoute::add('/ui/(?P<path>.*)',function($matches){
+        BasicRoute::add('/ui(?P<path>.*)',function($matches){
             $client=Helper::getCurrentClient();
             $path = Helper::getBuildPath();
             $compiler_config = (App::get('configuration'))['ext-compiler'];
-            echo  $path.PHP_EOL;
-            echo  $client.PHP_EOL;
             if (!file_exists($path) || !is_dir($path)){
-                echo "compile needed".PHP_EOL;
                 Helper::compile($compiler_config, $client );
             }
+            if (!file_exists($path) || !is_dir($path)){
+                throw new \Exception("Version could not be build");
+            }
+            if (($matches['path']=='')||($matches['path']=='/')) $matches['path']='index.html';
+            echo $path.'/'.$matches['path'];exit();
+            if (!file_exists($path.'/'.$matches['path'])){
+                exit(404);
+            }
+            self::readfile($path.'/'.$matches['path']);
             print_r($matches);exit();
         },['get'],false);
     }

3b5068b3205d0621b9fb5770db8b0496c8e3d8c4
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-19 08:40:56 +0100

    test ui path

---
+++ src/Routes/Ui.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tualo\Office\ExtJSCompiler\Routes;
+
+use Tualo\Office\Basic\TualoApplication as App;
+use Tualo\Office\Basic\Route as BasicRoute;
+use Tualo\Office\Basic\IRoute;
+use Tualo\Office\ExtJSCompiler\Helper;
+
+class Read implements IRoute{
+
+    public static function register(){
+        BasicRoute::add('/ui/(?P<path>.*)',function($matches){
+            print_r($matches);exit();
+        },['get'],true);
+    }
+}
--- src/functions.php
+++ src/functions.php
@@ -4,3 +4,4 @@ require_once "ICompiler.php";
 require_once "FileHelper.php";
 require_once "Helper.php";
 require_once "Routes/Compiler.php";
+require_once "Routes/Ui.php";

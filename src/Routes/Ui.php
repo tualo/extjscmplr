a28ab7395d33d543bf7516879dcc5142cf87413e
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-19 08:59:29 +0100

    compile

--- src/Routes/Ui.php
+++ src/Routes/Ui.php
@@ -13,10 +13,12 @@ class Ui implements IRoute{
         BasicRoute::add('/ui/(?P<path>.*)',function($matches){
             $client=Helper::getCurrentClient();
             $path = Helper::getBuildPath();
+            $compiler_config = (App::get('configuration'))['ext-compiler'];
             echo  $path.PHP_EOL;
             echo  $client.PHP_EOL;
             if (!file_exists($path) || !is_dir($path)){
-                echo "compile needed";
+                echo "compile needed".PHP_EOL;
+                Helper::compile($compiler_config, $client );
             }
             print_r($matches);exit();
         },['get'],false);

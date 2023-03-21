5cbd5749424d67affcbcb534dc4aa045ab0fa70a
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-21 09:43:42 +0100

    add attributes

--- src/Middlewares/Middleware.php
+++ src/Middlewares/Middleware.php
@@ -3,13 +3,17 @@
 namespace Tualo\Office\ExtJSCompiler\Middlewares;
 use Tualo\Office\Basic\TualoApplication;
 use Tualo\Office\Basic\IMiddleware;
+use Tualo\Office\ExtJSCompiler\AppJson;
 
 class Middleware implements IMiddleware{
     public static function register(){
         TualoApplication::use('extjscmplr',function(){
             try{
                 TualoApplication::javascript('extjscmplr_ext_before_load', './cmplr/ext_start.js',[],-10000);
-                TualoApplication::javascript('extjscmplr_bootstrap', './cmplr/bootstrap.js',[],1000000);
+                TualoApplication::javascript('extjscmplr_bootstrap', './cmplr/bootstrap.js',[],1000000,[
+                    'data-app'=>AppJson::get()['id'],
+                    'id'=>'microloader'
+                ]);
                 
 
             }catch(\Exception $e){

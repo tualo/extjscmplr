7d29d9b97760a9b996d59f7a7ec2bec76b71a1f1
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-22 10:40:18 +0100

    removed unnessesary code

--- src/functions.php
+++ src/functions.php
@@ -5,5 +5,4 @@ require_once "FileHelper.php";
 require_once "Helper.php";
 require_once "Routes/Compiler.php";
 require_once "Routes/Ui.php";
-require_once "Routes/Js.php";
 require_once "Middlewares/Middleware.php";

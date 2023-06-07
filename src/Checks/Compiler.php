780ec6d4130d65ced65724b7013c5dff87437e20
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-06-07 13:14:40 +0200

    testing Compiler in post setup

--- src/Checks/Compiler.php
+++ src/Checks/Compiler.php
@@ -6,6 +6,7 @@ use Tualo\Office\Basic\Middleware\Session;
 use Tualo\Office\Basic\PostCheck;
 use Tualo\Office\Basic\TualoApplication as App;
 
+use Tualo\Office\ExtJSCompiler\Helper;
 
 class Compiler extends PostCheck {
     public static function test(array $config){
@@ -29,6 +30,37 @@ class Compiler extends PostCheck {
             self::formatPrintLn(['green'],"\tsencha_compiler_sdk defined");
             if (!file_exists($config['ext-compiler']['sencha_compiler_sdk'])&&!is_dir($config['ext-compiler']['sencha_compiler_sdk'])){
                 self::formatPrintLn(['red'],"\tsencha_compiler_sdk not accessible");
+            }
+        }
+
+
+        $client=Helper::getCurrentClient();
+        $path = Helper::getCachePath();
+        
+        /*
+        echo "$client \n";
+        echo "$path \n";
+        */
+        if (file_exists($path) || !is_dir($path)){
+            self::formatPrintLn(['yellow'],"\tshould compiled first");
+            $prompt = [
+                "\t".'do you want to compile the "'.$client.'" now? [y|n|c]'
+            ];
+            while(in_array($line = readline(implode("\n",$prompt)),['yes','y','n','no','c'])){
+                if ($line=='c') exit();
+                if ($line=='y'){
+                    self::formatPrintLn(['blue'],"\t compiling");
+                    if (is_null(App::get('session'))){
+                        self::formatPrintLn(['red'],"\t not possible, no database");
+                    }else{
+                        Helper::compile($config['ext-compiler'], $client );
+                    }
+                    break;
+                }
+                if ($line=='n'){
+                    self::formatPrintLn(['yellow'],"\tthe compiler will start at the first time visiting the service");
+                    break;
+                }
             }
         }
         

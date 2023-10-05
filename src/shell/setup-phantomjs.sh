dfedf3a3ae9f8dbbbde82532b6c8d5dbdcd8da59
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-05 13:56:35 +0200

    add phantom js shell

---
+++ src/shell/setup-phantomjs.sh
@@ -0,0 +1,12 @@
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install nodejs -y
+nodejs --version
+npm install phantomjs
+npm set strict-ssl false
+npm install -g phantomjs-prebuilt --force
+/usr/bin/phantomjs --version

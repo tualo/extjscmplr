9530e65ce9a423647d79a7e85f5ed35ee675c8ba
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-18 21:23:04 +0200

    add config check

---
+++ src/Checks/Configuration.php
@@ -0,0 +1,28 @@
+<?php
+namespace Tualo\Office\ExtJSCompiler\Checks;
+
+use Tualo\Office\Basic\Middleware\Session;
+use Tualo\Office\Basic\PostCheck;
+use Tualo\Office\Basic\TualoApplication as App;
+
+
+class Configuration  extends PostCheck {
+    
+    public static function test(array $config){
+        $clientdb = App::get('clientDB');
+        if (is_null($clientdb)) return;
+
+        if(($cmd = App::configuration('ext-compiler','sencha_compiler_command',false))==false){
+            PostCheck::formatPrintLn(['red'],"\tsencha cmd not found");
+            PostCheck::formatPrintLn(['blue'],"\tcall `./tm configuration --section ext-compiler --key sencha_compiler_command --value $(which sencha)`");
+        }else{
+            exec($cmd.'',$output,$return_var);
+            if ($return_var!=0){
+                PostCheck::formatPrintLn(['red'],"\tsencha cmd *$cmd* is not callable ($return_var), try `npm install -g sencha-cmd`");
+            }else{
+                PostCheck::formatPrintLn(['green'],"\tsencha vesrion: ".implode(' ',$output));
+            }
+        }
+
+    }
+}
--- src/functions.php
+++ src/functions.php
@@ -8,3 +8,4 @@ require_once "Routes/Compiler.php";
 require_once "Routes/Ui.php";
 require_once "Middlewares/Middleware.php";
 require_once "Checks/Compiler.php";
+require_once "Checks/Configuration.php";
